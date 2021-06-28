bffeacfe3887bab0d7c45bf0948658693af6798c
Matthew Tran <39714315+SakanaMarket@users.noreply.github.com>
2021-06-27 18:02:48 -0700

    Debug + Menu UI
    -Debugged issue with WebGL compression
    
    -Added more UI functionality
    
    -Started on main level buttons

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class Manager : MonoBehaviour
 {
 
     [SerializeField] private Dictionary<string, int> Teams = new Dictionary<string, int>();
-    [SerializeField] private Text response;
+    //[SerializeField] private Text response;
     [SerializeField] private float TimeToAppear = 2f;
     [SerializeField] private float TimetoDisappear;
+    [SerializeField] GameObject playCanvas;
+    [SerializeField] GameObject scorecard;
 
     private void Update()
     {
-        if (response.enabled && (Time.time >= TimetoDisappear))
-        {
-            response.enabled = false;
-        }
+
     }
-    public void SetTeamNames()
+    public void SetTeamNames(Text r)
     {
         foreach (GameObject g in GameObject.FindGameObjectsWithTag("TeamInput"))
         {
@@ -28,9 +29,9 @@ public class Manager : MonoBehaviour
                 //Debug.Log(s);
                 Teams.Add(g.GetComponent<InputField>().text, 0);
             }
-            
+
         }
-        EnableResponse("Teams are Set!");
+        EnableResponse("Teams are Set!", r);
     }
 
     public void PrintTeams()
@@ -41,16 +42,61 @@ public class Manager : MonoBehaviour
         }
     }
 
-    public void ResetTeams()
+    public void ResetTeams(Text r)
     {
         Teams.Clear();
-        EnableResponse("Teams are Cleared!");
+        EnableResponse("Teams are Cleared!", r);
     }
 
-    public void EnableResponse(string s)
+    public void EnableResponse(string s, Text r)
     {
-        response.enabled = true;
-        response.text = s;
+        r.enabled = true;
+        r.text = s;
         TimetoDisappear = Time.time + TimeToAppear;
+        StartCoroutine(WaitResponse(r));
+    }
+
+    public IEnumerator WaitResponse(Text r)
+    {
+        yield return new WaitForSeconds(2);
+        if (r.enabled && (Time.time >= TimetoDisappear))
+        {
+            r.enabled = false;
+        }
+    }
+
+    public void Play(Text r)
+    {
+        if (Teams.Count > 0)
+        {
+            playCanvas.SetActive(true);
+            GameObject.FindGameObjectWithTag("MainCanvas").SetActive(false);
+        }
+        else
+        {
+            EnableResponse("Teams Must Be Setup!", r);
+        }
+    }
+
+    public void EnableCanvas(GameObject c)
+    {
+        c.SetActive(true);
+    }
+
+    public void DisableCanvas(GameObject c)
+    {
+        c.SetActive(false);
+    }
+
+    public void UpdateScore()
+    {
+        //string s = scorecard.GetComponent<TextMeshProUGUI>().text;
+        string s = "";
+        foreach (KeyValuePair<string, int> item in Teams.OrderBy(key=>key.Value))
+        {
+            s += item.Key + ":\t" + item.Value + "\n";
+        }
+
+        scorecard.GetComponent<TextMeshProUGUI>().text = s;
     }
 }
